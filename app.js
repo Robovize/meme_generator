68274823bd17d0365e73c25c0ad02efa293d03de
Robovize <56894839+Robovize@users.noreply.github.com>
2020-08-20 14:23:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const form = {
-    self: document.querySelector('#meme-form'),
     top: document.querySelector('#form-top'),
     bot: document.querySelector('#form-bot'),
     img: document.querySelector('#form-image'),
@@ -11,75 +10,67 @@ const form = {
 // Canvas meme functions
 ///////////////////////////////////
 
-const canvas = document.querySelector('.meme-canvas')
-const ctx = canvas.getContext('2d')
 
-function initCanvas() {
 
-    canvas.width = 600
-    canvas.height = 480
 
-}
+i = 0;
 
 function generateCanvasMeme() {
 
+    body = document.querySelector('body');
+    var x = document.createElement("canvas");
+    x.setAttribute("id", i);
 
-    initCanvas();
 
 
-    const img = new Image()
-    img.crossOrigin = "Anonymous"
-    img.onload = function() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        img.width = 600
-        img.height = 480
-        ctx.drawImage(img, 0, 0, img.width, img.height)
+    x.width = 600;
+    x.height = 480;
 
-        ctx.lineWidth = 5 // text stroke so its visible even on low contrast background
-        ctx.strokeStyle = 'black' // colour of stroke
-        ctx.fillStyle = 'white' // colour of text
-        ctx.textAlign = 'center' // position of text
-        ctx.lineJoin = 'round' // default value can look weird on small text, round fixes that
-        // const memeFont = `700 ${form.fontSize.value}px "${form.fontFamily.value}"`
-        const memeFont = `700 42px Aria` // we set bold, font size and font family
-        ctx.font = memeFont
 
-        const topText = String(form.top.value).toUpperCase()  // create uppercase
-        const botText = String(form.bot.value).toUpperCase()  // create uppercase
+    const ctx = x.getContext('2d');
 
-        const textX = canvas.width / 2 
-        const topY = canvas.height - (canvas.height / 30) * 29
-        const botY = canvas.height - (canvas.height / 30)
+    const img = new Image();
+    img.crossOrigin = "Anonymous";
+    img.onload = function () {
+        ctx.clearRect(0, 0, x.width, x.height);
+        img.width = 600;
+        img.height = 480;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
 
-        ctx.textBaseline = 'top'
-        ctx.strokeText(topText, textX, topY)
-        ctx.fillText(topText, textX, topY)
+        // ***********
+        // TEXT DESIGN
+        // ************
+        ctx.lineWidth = 5; // text stroke so its visible even on low contrast background
+        ctx.strokeStyle = 'black'; // colour of stroke
+        ctx.fillStyle = 'white'; // colour of text
+        ctx.textAlign = 'center'; // position of text
+        ctx.lineJoin = 'round'; // default value can look weird on small text, round fixes that
+        ctx.font = `700 42px Aria`; // we set bold, font size and font family
 
-        ctx.textBaseline = 'bottom'
-        ctx.strokeText(botText, textX, botY)
-        ctx.fillText(botText, textX, botY)
+
+
+        const topText = String(form.top.value).toUpperCase(); // create uppercase
+        const botText = String(form.bot.value).toUpperCase(); // create uppercase
+        const textX = x.width / 2; // center text
+        const topY = x.height - (x.height / 30) * 29; // put top text on the top of its baseline
+        const botY = x.height - (x.height / 30); // put bottom text on the bottom of its baseline
+
+
+
+        ctx.textBaseline = 'top'; // set baseline to the top
+        ctx.strokeText(topText, textX, topY);
+        ctx.fillText(topText, textX, topY);
+
+        ctx.textBaseline = 'bottom'; // set baseline to the bottom
+        ctx.strokeText(botText, textX, botY); // write text
+        ctx.fillText(botText, textX, botY);
     }
-    img.src = form.img.value
+    img.src = form.img.value;
+
+    body.appendChild(x);
+
+    i++;
 }
-initCanvas()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -91,6 +82,4 @@ initCanvas()
 
 
 
-form.generateButton.addEventListener('click', generateCanvasMeme)
-
-
+form.generateButton.addEventListener('click', generateCanvasMeme);
